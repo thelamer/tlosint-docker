e2bc2d250caaebfb7878ed32bd3e6995bdbea347
thelamer <ryankuba@gmail.com>
2024-08-01 14:49:38 -0400

    build7

--- ci-scripts/update-packages.sh
+++ ci-scripts/update-packages.sh
@@ -2,7 +2,7 @@
 set -e
 
 docker build --build-arg REPO=${GITHUB_REPOSITORY,,} -t ghcr.io/thelamer/tlosint-docker:latest .
-docker run --rm -it --entrypoint /package.sh ghcr.io/thelamer/tlosint-docker:latest > package_versions_check.txt
+docker run --rm --entrypoint /package.sh ghcr.io/thelamer/tlosint-docker:latest > package_versions_check.txt
 if [ ! -f 'package_versions.txt' ]; then
   mv package_versions_check.txt package_versions.txt
 elif ! diff -q package_versions_check.txt package_versions.txt; then
